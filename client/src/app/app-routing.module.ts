3f67aa1195c1d41cfc6b0475666578800ce08b7c
raymond-frontend <raymond.oyinlola@gmail.com>
2020-12-04 12:08:40 +0800

    create the clients folder structures

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./modules/posts/posts.module').then((m) => m.PostsModule),
+  },
+  {
+    path: 'core',
+    loadChildren: () =>
+      import('./modules/core/core.module').then((m) => m.CoreModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
