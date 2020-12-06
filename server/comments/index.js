2703477d727ea0dde7c2a318479c9598e102d7a5
raymond-frontend <raymond.oyinlola@gmail.com>
2020-12-06 13:37:04 +0800

    create the event bus service and emit events on posts creation

--- server/comments/index.js
+++ server/comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,7 +24,7 @@ app.get("/api/comments/posts/:id", (req, res) => {
 
 //Create a post comment
 
-app.post("/api/comments/posts/:id", (req, res) => {
+app.post("/api/comments/posts/:id", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   if (req.body.content.length < 1) {
@@ -32,9 +33,22 @@ app.post("/api/comments/posts/:id", (req, res) => {
   let comment = { id: commentId, content };
   const comments = commentsByPostId[req.params.id] || [];
   comments.push(comment);
+  //Send a new event
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
   commentsByPostId[req.params.id] = comments;
-  console.log(content);
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body);
+  res.send({});
 });
 
 app.listen(4001, () => {
--- server/posts/index.js
+++ server/posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,7 +21,17 @@ app.post("/api/posts", async (req, res) => {
   }
   let post = { id, title };
   posts.push(post);
+  //send out event
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: post,
+  });
   res.status(201).send(posts);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body);
+  res.send({});
 });
 
 app.listen(4000, () => {
